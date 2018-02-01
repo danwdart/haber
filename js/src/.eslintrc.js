72ca0c67a3d7052fd3de8821b78c296a7694d57f
Dan Dart <git@dandart.co.uk>
2018-02-01 00:16:42 +0000

    New techs and methods of doing things

---
+++ js/src/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "2018",
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "backtick"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- js/src/index.js
+++ js/src/index.js
@@ -5,14 +5,14 @@ import Foo from './components/foo';
 import Contacts from './components/contacts';
 
 
-const main = document.querySelector('main');
+const main = document.querySelector(`main`);
 
 ReactDOM.render(
     <div>
         <Foo name="Bob">
             <p>Extra content</p>
         </Foo>
-        <Contacts url="/contacts" />
+        <Contacts url="/mockdata/contacts.json" />
     </div>,
     main
 );
